a17240b5274a6a929657032fbb6bb51abec7993f
V3NCY <venceslavam@gmail.com>
2025-05-14 15:54:07 +0300

    fix with tempaltes

--- AI_Website_Generator/obj/Debug/net8.0-windows/AI_Website_Generator.AssemblyInfo.cs
+++ AI_Website_Generator/obj/Debug/net8.0-windows/AI_Website_Generator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Website_Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178e25d1d7c91f859a93459fba65b46f045e90d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69bab366057821b0f173594672a9f548fe0519af")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Website_Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Website_Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
