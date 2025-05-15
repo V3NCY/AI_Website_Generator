1eb4f7f35c10f8691bbbfb0734679517453d9b56
V3NCY <venceslavam@gmail.com>
2025-05-15 14:15:46 +0300

    logos added

--- AI_Website_Generator/ViewRequestsWindow.xaml.cs
+++ AI_Website_Generator/ViewRequestsWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Controls;
 using Newtonsoft.Json;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace AI_Website_Generator
 {
@@ -91,6 +92,78 @@ namespace AI_Website_Generator
                 File.WriteAllText(jsonPath, JsonConvert.SerializeObject(Requests, Formatting.Indented));
             }
         }
+
+        private void DeployTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button btn && btn.Tag is Request request)
+            {
+                try
+                {
+                    string templateFile = $"template{request.Template}.zip";
+                    string templatePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "templates", templateFile);
+                    string siteFolder = request.NewDomain.Replace(".", "").Replace("www", "");
+                    string targetDir = Path.Combine(@"C:\xampp\htdocs", siteFolder);
+
+                    if (!File.Exists(templatePath))
+                    {
+                        string htmlFile = $"template{request.Template}.html";
+                        string htmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "templates", htmlFile);
+
+                        if (!File.Exists(htmlPath))
+                        {
+                            MessageBox.Show($"Няма HTML файл за темплейт {request.Template}", "Липсващ файл", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+
+                        CreateWordPressThemeZip(htmlPath, templatePath, $"Template {request.Template}");
+                    }
+
+
+                    if (Directory.Exists(targetDir))
+                        Directory.Delete(targetDir, true);
+
+                    System.IO.Compression.ZipFile.ExtractToDirectory(templatePath, targetDir);
+                    MessageBox.Show($"Темплейтът е успешно деплойнат в:\n{targetDir}", "Готово", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = $"http://localhost/{siteFolder}",
+                        UseShellExecute = true
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Възникна грешка при деплой:\n" + ex.Message, "Грешка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        void CreateWordPressThemeZip(string htmlPath, string outputZipPath, string themeName)
+        {
+            string tempDir = Path.Combine(Path.GetTempPath(), $"wp_theme_{Guid.NewGuid()}");
+            Directory.CreateDirectory(tempDir);
+
+            string htmlContent = File.ReadAllText(htmlPath);
+
+            string indexPhp = htmlContent
+                .Replace("<!DOCTYPE html>", "<?php /* Template generated */ ?>\n<!DOCTYPE html>"); 
+
+            File.WriteAllText(Path.Combine(tempDir, "index.php"), indexPhp);
+
+            string styleCss = $@"/*
+            Theme Name: {themeName}
+            Author: Orak Academy
+            Version: 1.0
+            */";
+            File.WriteAllText(Path.Combine(tempDir, "style.css"), styleCss);
+
+            if (File.Exists(outputZipPath)) File.Delete(outputZipPath);
+            System.IO.Compression.ZipFile.CreateFromDirectory(tempDir, outputZipPath);
+            Directory.Delete(tempDir, true);
+        }
+
+
+
 
     }
 }
